a0f88e85bf36d0ef91898f61087b65555afac275
Esedo Fredrick <50068759+areaboy@users.noreply.github.com>
2022-04-20 21:33:56 +0100

    Add files via upload

---
+++ planet_savers_connect/footer_title.php
@@ -0,0 +1,9 @@
+﻿<?php
+
+$footer_tit1=  "Easily Post, Share or Monetize your <b>Foods(Food Waste), Composts and Plastic Can and bottle trash</b> by connecting People who want to share
+it with those people who need it thereby saving our planets from Climatic Change and Global Warming.";
+
+$footer_tit2 ="";
+
+$footer_tit3 ="";
+?>
---
+++ planet_savers_connect/header_title.php
@@ -0,0 +1,13 @@
+﻿<?php
+$header_tit=  "Planet Savers Connect";
+$invite_member ="Coming Soon";
+$poster ="Posts";
+$poster2 ="Help";
+$poweredby ="Graph For All Million Dollar Challenge";
+$heading ="<h3>Easily Post, Share or Monetize your <b>Foods(Food Waste), Composts and Trashed Cans and bottle trash</b> etc. by connecting People who want to share it with those people who need it thereby saving our planets from Climatic Change and Global Warming..</h3>
+
+
+
+
+Our applications awards more Points to appreciate those who gives help the most.";
+?>
